7264bde1f1f5b5c1066da28a6cc974ea0c2ff931
maulana48 <hidarizero4869@gmail.com>
2022-12-23 22:31:44 +0700

    adding .env with error

---
+++ app/config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config(); // this is important!, npm i --save dotenv
+module.exports = {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": process.env.DB_TEST_USERNAME,
+    "password": process.env.DB_TEST_PASSWORD,
+    "database": process.env.DB_TEST_DATABASE,
+    "host": process.env.DB_TEST_HOST,
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.DB_PROD_USERNAME,
+    "password": process.env.DB_PROD_PASSWORD,
+    "database": process.env.DB_PROD_DATABASE,
+    "host": process.env.DB_PROD_HOST,
+    "dialect": "postgres"
+  }
+};
